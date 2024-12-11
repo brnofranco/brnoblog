296dc9287fc010dd6ab8bedb4d980cd10cd37aca
Bruno Franco <brunofrancodemoraes@gmail.br>
2024-12-10 22:38:27 -0300

    adjust about data and styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,27 +12,30 @@ export default function About() {
 			<div className="h-full w-full flex-2">
 				<div className="flex h-full w-full flex-col justify-center gap-12">
 					<div className="flex h-full w-full flex-col gap-6 bg-postBody p-4 shadow-md xl:p-8">
-						<h2 className="text-3xl font-bold">Sobre</h2>
-						<p className="opacity-85">
-							O objetivo deste blog é compartilhar conhecimento de várias áreas. Estou fazendo um backup
-							escrito de alguns de meus estudos e pensamentos destes últimos meses. Ao longo que vou
-							postando novos conteúdos irei melhorando o site esteticamente e em performance.
+						<h2 className="text-3xl font-bold">Autor</h2>
+						<p className="text-justify opacity-85">
+							Estou em contato com o mundo da programação desde 2018 e de lá pra cá percebo que cada vez
+							mais sei que nada sei. Mas o que eu for aprendendo vou postando aqui.
+						</p>
+						<p className="text-justify opacity-85">
+							Sou desenvolvedor Full Stack Pleno com experiência em projetos corporativos de grande
+							escala. Tenho formação técnica em Informática para Internet pela ETEC Vasco Antônio
+							Venchiarutti e graduação em Análise e Desenvolvimento de Sistemas pela FATEC Jundiaí.
+						</p>
+						<p className="text-justify opacity-85">
+							Já trabalhei com Front-end utilizando React.js e Next.js, e Back-end com Node.js e Java.
+							Tenho prática na criação de Testes Unitários, Testes de Integração e Testes de Componente.
+							Trabalho em times que utilizam metodologia ágil Scrum, bem como colaboro com realização Code
+							Reviews, refinamentos técnicos e diagramas.
 						</p>
 					</div>
 
 					<div className="flex h-full w-full flex-col gap-6 bg-postBody p-4 shadow-md xl:p-8">
-						<h2 className="text-3xl font-bold">Autor</h2>
-						<p className="opacity-85">
-							Sou Bruno Franco, como o nome do blog já diz. Estou em contato com o mundo da programação
-							desde 2018 e de lá pra cá percebo que cada vez mais sei que nada sei. Mas o que eu for
-							aprendendo vou postando aqui.
-						</p>
-						<p className="opacity-85">
-							Sou desenvolvedor Full Stack com experiência em projetos corporativos de grande escala.
-							Tenho formação técnica em Informática para Internet e graduação em Análise e Desenvolvimento
-							de Sistemas. Já trabalhei com Front-end utilizando React.js e Next.js, e Back-end com
-							Node.js e Java. Prática na criação de Testes Unitários e Testes de Componente. Atuação em
-							times com metodologia ágil Scrum, bem como realização code reviews e refinamentos técnicos.
+						<h2 className="text-3xl font-bold">Sobre</h2>
+						<p className="text-justify opacity-85">
+							O objetivo deste blog é compartilhar conhecimento de várias áreas. Estou fazendo um backup
+							escrito de alguns de meus estudos e pensamentos destes últimos meses. Ao longo que vou
+							postando novos conteúdos irei melhorando o blog.
 						</p>
 					</div>
 				</div>
@@ -42,9 +45,9 @@ export default function About() {
 				<aside className="flex h-full w-full flex-col items-center gap-6 bg-postBody p-8 shadow-md">
 					<Image
 						src="https://github.com/brnofranco.png"
-						alt="Foto de bruno"
-						width={350}
-						height={350}
+						alt="Foto do Bruno"
+						width={280}
+						height={280}
 						className="rounded-full"
 					/>
 
@@ -59,15 +62,15 @@ export default function About() {
 
 					<div className="flex w-full items-center justify-center gap-6">
 						<Link href={links.linkedIn} target="_blank" title="LinkedIn">
-							<FaLinkedin color="#2cb67d" size="42" />
+							<FaLinkedin color="#2cb67d" size="36" />
 						</Link>
 
 						<Link href={links.gitHub} target="_blank" title="GitHub">
-							<FaGithub color="#2cb67d" size="42" />
+							<FaGithub color="#2cb67d" size="36" />
 						</Link>
 
 						<Link href={links.portfolio} target="_blank" title="Portfólio">
-							<GoLink color="#2cb67d" size="42" />
+							<GoLink color="#2cb67d" size="36" />
 						</Link>
 					</div>
 				</aside>
